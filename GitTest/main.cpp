915a1b62b45063c6767bfed135803102feae446b
Mingyu Kim <dpcm@naver.com>
2016-12-08 18:40:41 +0900

    GitHub에서 수정

--- GitTest/main.cpp
+++ GitTest/main.cpp
@@ -5,6 +5,6 @@ void main(void)
 	printf("Git Test\n");
 	printf("Branch Test\n");
 	printf("Master Branch\n");
-	printf("·ÎÄÃ ÀúÀå¼Ò master branch ¼öÁ¤");
+	printf("·ÎÄÃ ?ú?å¼Ò master branch ¼öÁ¤");
 	printf("modified on GitHub");
 }
